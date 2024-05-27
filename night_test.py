21bdf8109dd3ca9ebdb4cf6d735a1ec1f2410ee1
Karla Rangel <krangelhdz@gmail.com>
2024-05-27 20:35:02 +0100

    new Animal class created

---
+++ night_test.py
@@ -0,0 +1,10 @@
+class Animal():
+    def __init__(self, name, color):
+        self.name = name 
+        self.color = color
+
+    def getName(self):
+        return self.name
+    
+    def getColor(self):
+        return self.color
